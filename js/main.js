5b255495510bab9390ee1fa17a9e313edb6cee91
coding-hannah <70801762+coding-hannah@users.noreply.github.com>
2020-10-06 20:03:55 +0100

    java is pain

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-/**
- * Nothing to see here.
- */
+const myName='Hannah' ;
+console.log(myName);
+
+const myAge = 20;
+console.log(myAge);
+
+let myCity='Northampton'
+console.log(myCity);
+
+const person= {
+  name: 'Hannah',
+  age: 20,
+  hobbies: ['oboe','sleeping'],
+  isAlive:true,
+  address: {
+name: 'The Old Bakery',
+Street: 'High Street',
+City: 'Northamton'
+  }
+}
+
+console.log(person);
---
+++ js/top-five.js
@@ -0,0 +1,33 @@
+const projects =[{
+  title: 'Improving Double Reeds',
+  description: 
+  "I am really passionate about my university's double reed ensemble. During my year as Conductor of the group, I improved the availability of music, increased our repetoire and even helped us to put together virtual performances during lock down.",
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Oboe_modern.jpg/250px-Oboe_modern.jpg",
+  link: {
+  text: "Go listen to our virtual rendition of dancing queen!",
+  url: "https://www.facebook.com/watch/?ref=search&v=303338101037361&external_log_id=9ccec1b7-3129-4df7-8fd0-da956af06912&q=double%20reeds%20dancing"
+  }
+  },
+  {
+    title: 'Helping a local organisation',
+    description: 
+    "Over the summer, I worked in a team with 4 other UoN students. Together, we helped a local business to help carry out vital research in the local area. I was instrumental in the creation of our report for the client.",
+    image_url: "https://pbs.twimg.com/media/Ea21kEpWAAAuYRM?format=jpg&name=medium",
+    link: {
+    text: "LINK TO SOMETHING",
+    url: "LINK TO SOMETHING"
+    }
+    },
+    {
+      title: 'Law School Peer Mentoring',
+      description: 
+      "I have recently signed up to be a peer mentor to an incoming law student at my uni. I am very excited to guide a 1st year through what to do and what not to do!",
+      image_url: "https://www.whatcareerlive.co.uk/documents/blog-top-image/law-degree-ti-2110.jpg",
+      link: {
+      text: "Another link!",
+      url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+      }
+      }];
+  console.log (projects);
+
+ 
